c5a0d36d0e13a833ac6a2117c8cd3b49150fc5c5
yuuthod <yuuthod@gmail.com>
2020-06-30 22:45:50 +0900

    [components/MyPageEdit.vue] - CSS 수정

--- src/components/MyPageEdit.vue
+++ src/components/MyPageEdit.vue
@@ -1,83 +1,79 @@
 <template>
-  <form class="user_data">
+  <div class="container">
     <h1>내 정보 관리</h1>
-    <ul v-for="(obj, index) in userList" :key="index">
-      <li>
-        <div class="user_id">
-          <span>ID</span>
-          <span>{{ obj.userID }}</span>
-        </div>
-        <div class="user_nick">
-          <span>닉네임</span>
-          <span>{{ obj.userNick }}</span>
-        </div>
-        <div class="user_area">
-          <span>지역</span>
-          <!-- TODO switch to json data -->
+    <dl v-for="(obj, index) in userList" :key="index">
+      <div>
+        <dt>ID</dt>
+        <dd><input type="text" :value="obj.userID" readonly /></dd>
+      </div>
+      <div>
+        <dt>닉네임</dt>
+        <dd>
           <span>
-            <select>
-              <option selected>서울특별시</option>
-              <option>중랑구</option>
-              <option>종로구</option>
-            </select>
+            <input type="text" value="obj.userNick" />
           </span>
-          <span>
-            <select>
-              <option selected>광진구</option>
-              <option>중랑구</option>
-              <option>종로구</option>
-            </select>
-          </span>
-        </div>
-        <div class="user_height">
-          <span>신장</span>
-          <span>
-            <select>
-              <option>~ 150cm</option>
-              <option selected>160cm</option>
-              <option>170cm</option>
-              <option>180cm</option>
-              <option>190cm ~</option>
-            </select>
-          </span>
-        </div>
-        <div class="user_main_view">
-          <span>주시야</span>
-          <span>
-            <select>
-              <option selected>왼쪽</option>
-              <option>오른쪽</option>
-            </select>
-          </span>
-        </div>
-        <div class="user_preference">
-          <spsan>선호좌석</spsan>
-          <span>
-            <select>
-              <option selected>왼쪽</option>
-              <option>오른쪽</option>
-            </select>
-          </span>
-          <span>
-            <select>
-              <option>좌측</option>
-              <option selected>중앙</option>
-              <option>우측</option>
-            </select>
-          </span>
-        </div>
-        <div class="link">
-          <ul>
-            <li>수정</li>
-            <li class="logout">로그아웃</li>
-          </ul>
-          <ul>
-            <li>리뷰 쓰기</li>
-          </ul>
-        </div>
-      </li>
-    </ul>
-  </form>
+        </dd>
+      </div>
+      <div>
+        <dt>지역</dt>
+        <!-- TODO switch to json data -->
+        <dd>
+          <select>
+            <option selected>서울특별시</option>
+            <option>중랑구</option>
+            <option>종로구</option>
+          </select>
+          <select>
+            <option selected>광진구</option>
+            <option>중랑구</option>
+            <option>종로구</option>
+          </select>
+        </dd>
+      </div>
+      <div>
+        <dt>신장</dt>
+        <dd>
+          <select>
+            <option>~ 150cm</option>
+            <option selected>160cm</option>
+            <option>170cm</option>
+            <option>180cm</option>
+            <option>190cm ~</option>
+          </select>
+        </dd>
+      </div>
+      <div>
+        <dt>주시야</dt>
+        <dd>
+          <select>
+            <option selected>왼쪽</option>
+            <option>오른쪽</option>
+          </select>
+        </dd>
+      </div>
+      <div>
+        <dt>선호좌석</dt>
+        <dd>
+          <select>
+            <option selected>왼쪽</option>
+            <option>오른쪽</option>
+          </select>
+          <select>
+            <option>좌측</option>
+            <option selected>중앙</option>
+            <option>우측</option>
+          </select>
+        </dd>
+      </div>
+    </dl>
+    <div>
+      <button>수정</button>
+      <button>로그아웃</button>
+    </div>
+    <div>
+      <button>리뷰 쓰기</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -97,45 +93,71 @@ export default {
 </script>
 
 <style scoped>
-form.user_data {
-  margin-top: 100px;
-}
-form.user_data > h1 {
+div.container {
   text-align: center;
-  font-size: 2.8rem;
-  font-weight: 900;
-}
-form.user_data > div * {
-  margin: 10px;
-}
-form.user_data > ul {
   margin-top: 50px;
 }
-form.user_data > ul > li {
-  display: block;
-  width: 90%;
-  margin: 20px auto;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  font-weight: 600;
-  color: #202020;
+div.container > h1 {
+  font-size: 2.8rem;
 }
-
-div.link > ul {
-  display: flex;
-  text-align: center;
+dl {
+  margin-top: 30px;
+  text-align: left;
+  font-size: 1.3rem;
 }
-div.link > ul > li {
-  width: auto;
-  line-height: 10px;
-  padding: 50px;
+dl > div {
+  margin-top: 12px;
+}
+dl > div > dt,
+dl > div > dd {
+  display: inline-block;
+}
+dl > div > dt {
+  color: #888;
+  width: 30%;
+}
+dl > div > dd {
+  width: 70%;
+}
+dl > div > dd input {
+  width: 100%;
+  padding: 2px 5px;
+}
+dl > div > dd select {
+  width: 100%;
+  padding: 0 5px 5px 5px;
+}
+dl > div:nth-of-type(3n) > dd > select {
+  width: calc(50% - 5px);
+}
+dl > div:nth-of-type(3n) > dd > select:first-of-type {
+  margin-right: 10px;
+}
+/* 수정, 로그아웃 */
+div.container > div:nth-of-type(1) {
+  margin-top: 20px;
+}
+div.container > div > button {
+  margin-top: 20px;
+  padding: 10px 0;
   font-size: 1.5rem;
-  font-weight: 600;
-  color: #202020;
-  border-radius: 8px;
-  background-color: #ffc800;
+  background: #ffc800;
+  border-radius: 5px;
 }
-div.link > ul > li:not(:last-child) {
-  max-width: 50%;
+div.container > div:nth-of-type(1) > button {
+  display: inline-block;
+  width: calc(50% - 10px);
+}
+div.container > div:nth-of-type(1) > button:nth-of-type(1) {
+  margin-right: 10px;
+}
+div.container > div:nth-of-type(1) > button:nth-of-type(2) {
+  margin-left: 10px;
+  background: #cdcdcd;
+  color: #fff;
+}
+/* 리뷰 쓰기 */
+div.container > div:nth-of-type(2) > button {
+  width: 100%;
 }
 </style>
